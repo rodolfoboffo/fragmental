024cc67b9a11c44404e5a8ff4ed266a4286e46f4
Rodolfo Souza <rodolfoboffo@gmail.com>
2023-06-03 16:55:44 -0300

    Nice art from Basins

--- fragmental.js
+++ fragmental.js
@@ -38,24 +38,24 @@ class Complex {
 };
 
 class NewtonRhapsonMethod {
-    constructor(epsilonToResult=1E-8, epsilonToDerivative=1E-12, maxIter=200, func=null) {
-        this.epsilonToResult = epsilonToResult;
-        this.epsilonToDerivative = epsilonToDerivative;
-        this.maxIter = maxIter;
-        this.func = func;
+    constructor(options) {
+        this.epsilonToResult = options && options.epsilonToResult || 1E-8;
+        this.epsilonToDerivative = options && options.epsilonToDerivative || 1E-12;
+        this.maxIter = options && options.maxIter || 200;
+        this.function = options && options.func || null;
     }
 
-    setFunc(f) {
-        this.func = f;
+    setFunction(f) {
+        this.function = f;
     }
 
     iterate(initialGuess, steps=this.maxIter) {
-        let fInitGuess = this.func(initialGuess);
+        let fInitGuess = this.function(initialGuess);
         let initGuessPlusEps = initialGuess.plus(Complex.random(length=this.epsilonToDerivative));
-        let finitGuessPlusEps = this.func(initGuessPlusEps);
+        let finitGuessPlusEps = this.function(initGuessPlusEps);
         let l = fInitGuess.negate().divide(finitGuessPlusEps.minus(fInitGuess))
         let newGuess = initialGuess.plus(l.multiply(initGuessPlusEps.minus(initialGuess)))
-        let fNewGuess = this.func(newGuess);
+        let fNewGuess = this.function(newGuess);
         let satisfies = fNewGuess.norm() <= this.epsilonToResult
         if (satisfies) {
             return [newGuess, steps];
@@ -70,14 +70,24 @@ class NewtonRhapsonMethod {
 }
 
 class NewtonBasins {
-    constructor() {
-        this.zoom = 1.0;
-        this.offset = 0, 0;
+    constructor(options) {
+        this.zoom = options && options.zoom || 1.0E-1;
+        this.center = options && options.center || new Complex(-3.0, 2.0);
         this.newtonRhapson = new NewtonRhapsonMethod();
+        this.epsilonToRoots = options && options.epsilonToRoots || 1E-5;
+        this.colors = [
+                        [[255, 164, 164, 255], [217, 0, 0, 255]], // [210, 0, 0, 255], 
+                        [[255, 203, 164, 255], [240, 102, 0, 255]], // [232, 99, 0, 255]
+                        [[255, 245, 193, 255], [255, 219, 17, 255]], 
+                        [[197, 255, 145, 255], [94, 198, 0, 255]]
+                    ];
+        this.roots = [];
+        this.imageArray = null;
     }
 
-    setFunc(f) {
-        this.newtonRhapson.setFunc();
+    setFunction(f) {
+        this.newtonRhapson.function = f;
+        this.roots = [];
     }
 
     setCanvas(canvas) {
@@ -89,6 +99,10 @@ class NewtonBasins {
         this.ctx = null;
         if (canvas != null)
             this.ctx = canvas.getContext("2d");
+            this.imageArray = new Array(canvas.width);
+            for (let col = 0; col < canvas.width; col++) {
+                this.imageArray[col] = new Array(canvas.height);
+            }
     }
 
     clear() {
@@ -96,22 +110,92 @@ class NewtonBasins {
             this.ctx.reset();
         }
     }
+
+    getComplexCoordFromPixel(col, row, width, height, zoom, center) {
+        let step = 1.0 / zoom;
+        let cornerRe = center.re - (width * step) / 2;
+        let cornerIm = center.im - (height * step) / 2;
+        let re = cornerRe + col * step;
+        let im = cornerIm + (height-row) * step;
+        return new Complex(re, im);
+    }
+
+    upsertRootIndex(newRoot, roots, epsToCompare) {
+        if (newRoot == null)
+            return null;
+        for (let i = 0; i < roots.length; i++) {
+            let distance = newRoot.minus(roots[i].root).norm();
+            if (distance <= 2*epsToCompare) {
+                return i;
+            }
+        }
+        roots.push({root:newRoot});
+        return roots.length - 1;
+    }
+
+    calculateBasins() {
+        this.roots = [];
+        let [width, height] = [this.ctx.canvas.width, this.ctx.canvas.height]
+        for (let col = 0; col < width; col++) {
+            for (let row = 0; row < height; row++) {
+                let initGuess = this.getComplexCoordFromPixel(col, row, width, height, this.zoom, this.center);
+                let [root, stepsTaken] = this.newtonRhapson.iterate(initGuess);
+                let rootIndex = this.upsertRootIndex(root, this.roots, this.epsilonToRoots);
+                this.imageArray[col][row] = {steps: stepsTaken, rootIndex:rootIndex};
+            }
+        }
+    }
+
+    createImageData() {
+        let [width, height] = [this.ctx.canvas.width, this.ctx.canvas.height]
+        let imgData = this.ctx.createImageData(width, height);
+        let bytesPerPixel = 4;
+        for (let col = 0; col < width; col++) {
+            for (let row = 0; row < height; row++) {
+                let rootIndex = this.imageArray[col][row].rootIndex;
+                let color = [0, 0, 0, 255];
+                if (rootIndex != null) {
+                    let [colorStart, colorStop] = this.colors[rootIndex];
+                    let gradient = this.imageArray[col][row].steps / this.newtonRhapson.maxIter;
+                    for (let k = 0; k < 4; k++) {
+                        color[k] = colorStart[k] + (colorStop[k] - colorStart[k]) * gradient;
+                    }
+                }
+                for (let k = 0; k < 4; k++) {
+                    imgData.data[row * (width * bytesPerPixel) + col * bytesPerPixel + k] = color[k];
+                }
+            }
+        }
+        return imgData;
+    }
+
+    flush(imgData) {
+        this.ctx.putImageData(imgData, 0, 0);
+    }
+
+    redraw() {
+        if (this.ctx == null || this.newtonRhapson.function == null)
+            return;
+        this.calculateBasins();
+        let imgData = this.createImageData();
+        this.flush(imgData);
+    }
 };
 
 function main(canvasName) {
     let canvas = document.getElementById(canvasName)
     n.setCanvas(canvas);
-    n.setFunc(x => (x.minus(new Complex(1))).multiply(x.minus(new Complex(-2))));
+    n.setFunction((x => (x.minus(new Complex(1.0))).multiply(x.minus(new Complex(-2.0))).multiply(x.minus(new Complex(-3.0, 2.0)))));
 }
 
 function redraw() {
-    n.clear()
+    n.redraw();
 }
 
 var n = new NewtonBasins();
-var nrm = new NewtonRhapsonMethod();
-let f = x => (x.minus(new Complex(1))).multiply(x.minus(new Complex(-2)));
-//console.log(f(new Complex(3, 2)))
-nrm.setFunc(f);
-const [root, nSteps] = nrm.iterate(new Complex(36, 5));
-console.log(root);
+// var nrm = new NewtonRhapsonMethod();
+// let f = x => (x.minus(new Complex(1))).multiply(x.minus(new Complex(-2)));
+// console.log(f(new Complex(3, 2)))
+// nrm.setFunction(f);
+// const [root, nSteps] = nrm.iterate(new Complex(36, 5));
+// console.log(root);
